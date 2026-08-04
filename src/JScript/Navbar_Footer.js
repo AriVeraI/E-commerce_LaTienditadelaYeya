894bc50de6e02066190ef2ebc4cdce9175e98f61
AriVeraI <verariadn@gmail.com>
2026-08-04 16:10:08 -0600

    Cambios en navbar de admin y catalogo

--- src/JScript/Navbar_Footer.js
+++ src/JScript/Navbar_Footer.js
@@ -8,8 +8,18 @@ fetch('../Components/navbar1.html')
     })
     .catch(error => console.error('Hubo un error al cargar el navbar:', error));
 
+// Usamos fetch para buscar el archivo HTML del navbar
+fetch('../Components/navbar2.html')
+    .then(response => response.text()) // Convertimos la respuesta a texto plano
+    .then(data => {
+        // Seleccionamos el div del index y le asignamos el HTML obtenido
+        document.getElementById('navbar2').innerHTML = data;
+        console.log("Si se cargo chido")
+    })
+    .catch(error => console.error('Hubo un error al cargar el navbar:', error));
 
-    // Usamos fetch para buscar el archivo HTML del footer
+
+// Usamos fetch para buscar el archivo HTML del footer
 fetch('../Components/footer.html')
     .then(response => response.text()) // Convertimos la respuesta a texto plano
     .then(data => {
