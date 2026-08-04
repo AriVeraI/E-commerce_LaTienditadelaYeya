a76fc68722bd18952b08c028086aaac7ede5eea9
Ernesto-Nava-Hernandez <motorclub128@gmail.com>
2026-08-04 17:12:56 -0600

    Se creo el script para el formulario de agregar producto

---
+++ src/JScript/Product-Add.js
@@ -0,0 +1,14 @@
+//funcion para captura de datos del formulario
+const mainForm = () => {
+
+}
+
+
+//objeto para almacenar los productos
+const addProducto = {};
+
+
+//funcion para convertir a JSON el objeto
+const jsonProduct = () => {
+
+}
