96a06b2b168f39fc0587d56fa9ae873448770e3c
Essa-Cortez <essawcortez@outlook.com>
2026-08-10 15:42:52 -0600

    Rama Essaw actualizada

--- src/JScript/Navbar_Footer.js
+++ src/JScript/Navbar_Footer.js
@@ -1,5 +1,22 @@
+
+
+
 // Usamos fetch para buscar el archivo HTML del navbar
 fetch('../Components/navbar1.html')
+    .then(response => response.text())
+    .then(data => {
+        const nav1 = document.getElementById('navbar1');
+        if (nav1) { // Solo inyecta si existe el elemento en la página
+            nav1.innerHTML = data;
+        }
+    })
+    .catch(error => console.error('Error al cargar navbar1:', error));
+
+
+
+
+
+fetch('../../Components/navbar1.html')
     .then(response => response.text()) // Convertimos la respuesta a texto plano
     .then(data => {
         // Seleccionamos el div del index y le asignamos el HTML obtenido
